e86435efbef308abdc697be1329f8a133ed55a3e
Binay <binaydas1123@gmail.com>
2024-10-13 22:38:35 +0530

    New route for deleting a transaction

--- backend/routes/transactionRoutes.js
+++ backend/routes/transactionRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const transactionModel = require('../models/transaction');
 
 router.post('/transactions', async (req, res) => {
-    const {name, amount, dateTime, description} = req.body;
-    const newTransaction = await transactionModel.create({name, amount, dateTime, description});
+    const { name, amount, dateTime, description } = req.body;
+    const newTransaction = await transactionModel.create({ name, amount, dateTime, description });
     res.json(newTransaction);
     console.log('New transaction added:', newTransaction);
 
@@ -14,6 +14,20 @@ router.get('/transactions', async (req, res) => {
     res.json(transactions);
 });
 
+router.delete('/transaction/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const deleteTransaction = await transactionModel.findByIdAndDelete(id);
 
+        if (!deleteTransaction) {
+            return res.status(404).json({ message: 'Transaction not found!' });
+        }
+
+        res.status(200).json({ message: 'Transaction deleted successfully' });
+
+    } catch (error) {
+        res.status(500).json({ message: 'Server error while deleting transaction!', error });
+    }
+})
 
 module.exports = router;
